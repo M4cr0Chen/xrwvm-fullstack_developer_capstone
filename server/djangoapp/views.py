42bfd107d28a564550991dd0c01283f02697a052
Zhenghong Chen <macrochen05@gmail.com>
2024-08-20 06:45:00 -0400

    add Dockerfile entrypoint.sh

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,7 +16,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments  # , post_review
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -130,20 +130,20 @@ def get_dealer_details(request, dealer_id):
 
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
 def add_review(request):
-    if request.user.is_anonymous is False:
-        # data = json.loads(request.body)
+    if (request.user.is_anonymous is False):
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({
-                "status": 401,
-                "message": "Error in posting review"
-            })
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
 
 
 def get_cars(request):
